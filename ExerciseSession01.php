eb4c9fc86bf55fb81df7e040ae455ebf24ab6f68
kaimex9 <kaimexalabau@gmail.com>
2024-10-03 18:15:05 +0200

    finalsave

--- ExerciseSession01.php
+++ ExerciseSession01.php
@@ -32,14 +32,13 @@
             echo "ERROR: No puedes dejar casillas en blanco";   //Si alguna de las casillas esta vacia saltara este error
         } else {
             echo 'Worker: ' . $_POST["name"] . '<br>';
-            echo 'Product: ' . $_POST["product"] . '<br>';
             if ($_POST["product"] == "Soft Drink") {         //Dependiendo del tipo de producto se almacena en una variable u otra
                 $_SESSION['SD'] += (int) $_POST["amount"];
-                echo 'Units of ' . $_POST["product"] . ': ' . $_SESSION["SD"];
             } elseif ($_POST["product"] == "Milk") {
                 $_SESSION['M'] += (int) $_POST["amount"];
-                echo 'Units of ' . $_POST["product"] . ': ' . $_SESSION["M"];
             }
+            echo 'Units of Soft Drink: ' . $_SESSION["SD"];
+            echo '<br>Units of Milk: ' . $_SESSION["M"];
         }
     } elseif (isset($_POST["remove"])) {                     //Funcion de lo que ocurre al pulsar el boton "remove"
         if ($_POST["name"] == null && $_POST["amount"] == null) {   //Si alguna de las casillas esta vacia saltara este error
@@ -48,14 +47,15 @@
             echo "ERROR: El Stock de un producto no puede ser menor que 0";
         } else {
             echo 'Worker: ' . $_POST["name"] . '<br>';
-            echo 'Product: ' . $_POST["product"] . '<br>';
             if ($_POST["product"] == "Soft Drink") {        //Dependiendo del tipo de producto se almacena en una variable u otra
                 $_SESSION['SD'] -= (int) $_POST["amount"];
-                echo 'Units of ' . $_POST["product"] . ': ' . $_SESSION["SD"];
+
             } elseif ($_POST["product"] == "Milk") {
                 $_SESSION['M'] -= (int) $_POST["amount"];
-                echo 'Units of ' . $_POST["product"] . ': ' . $_SESSION["M"];
+
             }
+            echo 'Units of Soft Drink: ' . $_SESSION["SD"];
+            echo '<br>Units of Milk: ' . $_SESSION["M"];
         }
     }
     ?>
